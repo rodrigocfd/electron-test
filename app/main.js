e78fe680b9f4c3cdee6d90de99101d0fa3ccee7c
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2019-01-09 13:34:21 -0200

    Adding good ol' jQuery.

--- app/main.js
+++ app/main.js
@@ -4,24 +4,22 @@ const {ipcRenderer} = require('electron');
 const {dialog} = require('electron').remote;
 const fs = require('fs');
 
-document.addEventListener('DOMContentLoaded', () => {
-	document.getElementById('info').textContent = `Node ${process.version}`;
+$(document).on('DOMContentLoaded', () => {
+	$('#info').text(`Node ${process.version}`);
 
-	const btn = document.getElementById('btn');
-	btn.addEventListener('click', e => {
+	$('#btn').on('click', e => {
 		console.log('Clicked', e);
 		ipcRenderer.send('show-msg', {msg: 'Hello dude'});
 	});
 
-	const btnLoadFile = document.getElementById('loadFile');
-	btnLoadFile.addEventListener('click', e => {
+	$('#loadFile').on('click', e => {
 		dialog.showOpenDialog({
 			properties: ['openFile']
 		}, filePaths => {
 			if (filePaths !== undefined) {
 				fs.readFile(filePaths[0], 'utf-8', (err, data) => {
 					if (!err) {
-						document.getElementById('fileOut').value = data;
+						$('#fileOut').val(data);
 					}
 				});
 			}
